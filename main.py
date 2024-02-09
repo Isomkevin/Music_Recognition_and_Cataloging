fc58e22b7d9d3600a3a34a3fd76ecbbb6eb87ac4
Isomkevin <47275702+Isomkevin@users.noreply.github.com>
2024-02-09 19:47:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+from shazamio import Shazam
+from helper import get_required_song_info
+from songMetadata import editMusicTag, get_music_metadata
+
+
+async def main(musicFile):
+  """
+  Recognises the song and returns the metadata
+
+  Args:
+  - musicTrack: The path to the music file
+
+  Returns:
+  - A Dictionary with the metadata of the song created by Shazam 
+  """
+
+  shazam = Shazam()
+  out = await shazam.recognize_song(musicFile)
+  song_info = get_required_song_info(out)
+  editMusicTag(song_info, musicFile)
+  print(f"{musicFile} Catalouged!!/n")
+  print(get_music_metadata(musicFile))
+
+
+if __name__ == '__main__':
+  musicTrack = u'music_folder/Bebe Rexha - Sacrifice [Official Music Video].mp3'
+  loop = asyncio.get_event_loop()
+  loop.run_until_complete(main(musicTrack))
---
+++ songMetadata.py
@@ -0,0 +1,61 @@
+import eyed3
+
+
+def get_music_metadata(track):
+
+  def get_genre():
+    genre_object = eyed3.id3.Genre()
+    genre_name = genre_object.name
+    return genre_object
+
+  audio = eyed3.load(track)
+  audio_data = {
+      "Title": audio.tag.title,
+      "Artist": audio.tag.artist,
+      "Album": audio.tag.album,
+      "Publisher": audio.tag.publisher,
+      "Genre": f"{get_genre()}",
+      "Release_date": audio.tag.release_date
+  }
+  return audio_data
+
+
+def editMusicTag(parameters, file_path):
+  """
+  Edit the tags of a music file specified by the file path using the provided
+  parameters.
+
+  Args:
+      parameters (dict): A dictionary containing the tag parameters to edit.
+                         Supported keys: 'TITLE', 'ARTIST', 'ALBUM',
+                         'PUBLISHER', 'RELEASE_DATE'.
+                         Each key should map to the corresponding tag value.
+      file_path (str): The file path of the music file to be edited.
+
+  Raises:
+      ValueError: If the file path is invalid or if the specified tag
+      parameters are not supported.
+
+  Returns:
+      None
+  """
+  audio_file = eyed3.load(file_path)
+  if not audio_file.tag:
+    audio_file.initTag()
+  if parameters['trackTitle']:
+    audio_file.tag.title = parameters['trackTitle']
+  if parameters['trackArtist']:
+    audio_file.tag.artist = parameters['trackArtist']
+  if parameters['Album']:
+    audio_file.tag.album = parameters['Album']
+  if parameters['Label']:
+    audio_file.tag.publisher = parameters['Label']
+  if parameters['Released']:
+    audio_file.tag.release_date = parameters['Released']
+
+  audio_file.tag.save()
+
+
+if __name__ == '__main__':
+  musicTrack = 'The Chainsmokers - Side Effects (Official Video) ft. Emily Warren.mp3'
+  print(get_music_metadata(musicTrack))
